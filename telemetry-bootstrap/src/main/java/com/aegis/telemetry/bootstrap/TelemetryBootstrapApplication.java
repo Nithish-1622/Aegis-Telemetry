546de2cbf79238cc0fc75bd5abb9a1367c80965c
Nithish S <nithish.s.1622@gmail.com>
2026-08-05 12:43:45 +0530

    feat(bootstrap): add telemetry bootstrap application

---
+++ telemetry-bootstrap/src/main/java/com/aegis/telemetry/bootstrap/TelemetryBootstrapApplication.java
@@ -0,0 +1,12 @@
+package com.aegis.telemetry.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemetryBootstrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelemetryBootstrapApplication.class, args);
+    }
+}
