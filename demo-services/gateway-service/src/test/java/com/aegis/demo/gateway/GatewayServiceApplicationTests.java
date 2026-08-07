53a66f06a6a7cc65c55e3c41035409386e7ff504
Nithish S <nithish.s.1622@gmail.com>
2026-08-07 12:00:53 +0530

    test(demo): add end-to-end integration tests

---
+++ demo-services/gateway-service/src/test/java/com/aegis/demo/gateway/GatewayServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.aegis.demo.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = {"spring.main.web-application-type=none"})
+class GatewayServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ demo-services/payment-service/src/test/java/com/aegis/demo/payment/PaymentServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.aegis.demo.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = {"spring.main.web-application-type=none"})
+class PaymentServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
